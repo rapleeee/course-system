6ecefc72298bb60f1566fdf41faf00f02f2f4a2d
rapleeee <raflimaulanaaaa@gmail.com>
2025-06-20 20:38:46 +0700

    refactor: update env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,16 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCzNwWPIr1fZ3iOPrdPf4B3ZQgGklrJ7U0",
-  authDomain: "mentora-07.firebaseapp.com",
-  projectId: "mentora-07",
-  storageBucket: "mentora-07.firebasestorage.app",
-  messagingSenderId: "207237322799",
-  appId: "1:207237322799:web:c17b8ef76d8f0cda180eda",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-1F98G63YKM"
 };
 
